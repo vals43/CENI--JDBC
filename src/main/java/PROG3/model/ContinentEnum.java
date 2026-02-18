8042d11d1b4d7f67a6637f67c7dd18e0da3a38f5
Vals43 <hei.teddy.3@gmail.com>
2026-02-18 10:34:25 +0300

    first commit

--- src/main/java/PROG3/model/ContinentEnum.java
+++
@@ -1,8 +0,0 @@
-package PROG3.model;
-
-public enum ContinentEnum {
-    AFRICA,
-    EUROPA,
-    ASIA,
-    AMERICA
-}
--- src/main/java/PROG3/model/PlayerPositionEnum.java
+++
@@ -1,5 +0,0 @@
-package PROG3.model;
-
-public enum PlayerPositionEnum {
-    GK,  DEF,  MIDF,  STR
-}
