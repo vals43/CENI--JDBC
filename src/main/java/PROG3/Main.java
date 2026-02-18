d0f85d5cba4f63045d8e0fb56b1c8c1ff42b21e2
Vals43 <hei.teddy.3@gmail.com>
2026-02-18 10:39:59 +0300

    first commit

--- src/main/java/PROG3/Main.java
+++ src/main/java/PROG3/Main.java
@@ -1,40 +1,7 @@
 package PROG3;
 
-import PROG3.DB.DBConnection;
-import PROG3.model.Team;
-import PROG3.service.DataRetriever;
 
 public class Main {
     public static void main(String[] args) {
-        DataRetriever data = new DataRetriever();
-        DBConnection db = new DBConnection();
-
-        try {
-            db.getDBConnection();
-            Team team = data.findTeamById(1);
-            System.out.println("=== TEST findTeamById ===");
-            try {
-                System.out.print("Total buts équipe: ");
-                System.out.print(team.getPlayersGoals());
-            } catch (RuntimeException e) {
-                System.out.print("Erreur: " + e.getMessage());
-            }
-
-            System.out.println("\n=== TEST saveTeam ===");
-            try{
-                if (team != null) {
-                    Team saved = data.saveTeam(team);
-
-                    System.out.println("Équipe après sauvegarde : " + saved.getName());
-                    System.out.println("Total buts après sauvegarde : " + saved.getPlayersGoals());
-                }
-            } catch (RuntimeException e) {
-                System.out.print("Erreur: " + e.getMessage());
-            }
-
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
